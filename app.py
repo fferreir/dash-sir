54237426869e5fac1f649e22275160dc43c0c6bc
Fernando Ferreira <fernandoferreira@usp.br>
2025-06-28 17:36:16 +0000

    Ajustadas taxas e perguntas

--- app.py
+++ app.py
@@ -38,12 +38,12 @@ parametros = dcc.Markdown(
 )
 cond_inicial = dcc.Markdown(
     '''
-    * coeficiente de transmissão: $$\\beta=0.5 \\space ano^{-1}$$
-    * taxa de recuperação: $$\\gamma=0.01 \\space ano^{-1}$$
-    * taxa de vacinação: $$\\nu_0=5.0 \\space ano^{-1}$$, $$t_1=2$$ e $$t_2=5$$
-    * texa de perda de imunidade: $$\\delta=1 \\space ano^{-1}$$
-    * taxa de natalidade (= taxa de mortalidade): $$\\alpha=\\mu=0.15 \\space ano^{-1}$$
-    * condições iniciais: $$S(0)=100$$, $$I(0)=1$$, $$R(0)=0$$
+    * coeficiente de transmissão: $$\\beta=80 \\space ano^{-1}$$
+    * período infeccioso: 15 dias
+    * taxa de vacinação: $$\\nu_0=0.0 \\space ano^{-1}$$, $$t_1=2$$ e $$t_2=5$$
+    * período de imunidade: 6 meses
+    * taxa de natalidade (= taxa de mortalidade): $$\\alpha=\\mu=0.10 \\space ano^{-1}$$
+    * condições iniciais: $$S(0)=99$$, $$I(0)=1$$, $$R(0)=0$$
     ''', mathjax=True
 )
 
@@ -51,13 +51,12 @@ perguntas = dcc.Markdown(
     '''
     1. Para a configuração inicial dos parâmetros, em quanto tempo
     se atinge o estado de equilíbrio? Estime a proporção de animais positivos e o número de animais infectados.
-    2. Responda às mesmas questões do item 1, considerando uma taxa de recuperação de $$\\gamma=1 \\space ano^{-1}$$
-    e $$\\beta=0.05 \\space ano^{-1}$$.
-    3. Retornando à configuração inicial de parâmetros, adote $$\\nu_0=1.0 \\space ano^{-1}$$ e
-    $$\\gamma=0.01 \\space ano^{-1}$$. Qual o efeito sobre o número de suscetíveis de se abandonar o programa de vacinação?
+    2. Responda às mesmas questões do item 1, considerando um período infeccioso de $$30 \\space \\text{dias}$$
+    e $$\\beta=20 \\space ano^{-1}$$.
+    3. Retornando à configuração inicial de parâmetros, adote $$\\nu_0=2.0 \\space ano^{-1}$$ e período infeccioso
+     de 6 dias. O que acontece com o número de suscetíveis, infectados e recuperados? Qual o efeito sobre o número de suscetíveis de se abandonar o programa de vacinação?
     Como fica o número de animais infectados?
-    4. Reduzindo o período infeccioso, por meio do aumento da taxa de recuperação para $$\\gamma=0.1 \\space ano^{-1}$$ e
-    diminuindo a taxa de vacinação para $$\\nu_0=0.2 \\space ano^{-1}$$, observe o que ocorre com o número de suscetíveis
+    4. Aumentando o período de imunidade para 12 meses e aumentando a taxa de vacinação para $$\\nu_0=10 \\space ano^{-1}$$, observe o que ocorre com o número de suscetíveis
     e infectados.
     ''', mathjax=True
 )
@@ -88,21 +87,21 @@ ajuste_condicoes_iniciais = html.Div(
             html.Div(
                 [
                     dbc.Label(dcc.Markdown('''$$S$$ total de suscetíveis''', mathjax=True), html_for="s_init"),
-                    dcc.Slider(id="s_init", min=0, max=10000, value=10000, tooltip={"placement": "bottom", "always_visible": False}),
+                    dcc.Slider(id="s_init", min=0, max=100, value=99, tooltip={"placement": "bottom", "always_visible": False}),
                 ],
                 className="m-2",
             ),
             html.Div(
                 [
                     dbc.Label(dcc.Markdown('''$$I$$ total de infectados ''', mathjax=True), html_for="i_init"),
-                    dcc.Slider(id="i_init", min=0, max=10000, value=10, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
+                    dcc.Slider(id="i_init", min=0, max=100, value=1, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
                 ],
                 className="m-1",
             ),
             html.Div(
                 [
                     dbc.Label(dcc.Markdown('''$$R$$ total de recuperados ''', mathjax=True), html_for="r_init"),
-                    dcc.Slider(id="r_init", min=0, max=10000, value=0, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
+                    dcc.Slider(id="r_init", min=0, max=100, value=0, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
                 ],
                 className="m-1",
             ),
@@ -124,34 +123,34 @@ ajuste_parametros = html.Div(
             html.Div(
                 [
                     dbc.Label(dcc.Markdown('''$$\\beta$$ coeficiente de transmissão ''', mathjax=True), html_for="beta"),
-                    dcc.Slider(id="beta", min=60, max=100, value=80, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
+                    dcc.Slider(id="beta", min=0, max=100, value=80, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
                 ],
                 className="m-1",
             ),
             html.Div(
                 [
-                    dbc.Label(dcc.Markdown('''$$\\gamma$$ taxa de recuperação ''', mathjax=True), html_for="gamma"),
-                    dcc.Slider(id="gamma", min=18, max=36, value=20, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
+                    dbc.Label(dcc.Markdown('''Período infeccioso (dias) ''', mathjax=True), html_for="gamma"),
+                    dcc.Slider(id="gamma", min=6, max=30, value=15, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
                 ],
                 className="m-1",
             ),
             html.Div(
                 [
-                    dbc.Label(dcc.Markdown('''$$\\delta$$ taxa de perda de imunidade ''', mathjax=True), html_for="delta"),
-                    dcc.Slider(id="delta", min=0.5, max=2, value=1, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
+                    dbc.Label(dcc.Markdown('''Período de imunidade (meses) ''', mathjax=True), html_for="delta"),
+                    dcc.Slider(id="delta", min=1, max=24, value=6, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
                 ],
                 className="m-1",
             ),
             html.Div(
                 [
                     dbc.Label(dcc.Markdown('''$$\\nu_0$$ taxa de vacinação instantânea''', mathjax=True), html_for="nu"),
-                    dcc.Slider(id="nu", min=0, max=10, value=5, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
+                    dcc.Slider(id="nu", min=0, max=10, value=0, tooltip={"placement": "bottom", "always_visible": False}, className="card-text"),
                 ],
                 className="m-1",
             ),
             html.Div(
                 [
-                    dbc.Label(dcc.Markdown('''Período de aplicação da vacina''', mathjax=True), html_for="vacinacao"),
+                    dbc.Label(dcc.Markdown('''Período de aplicação da vacina (anos)''', mathjax=True), html_for="vacinacao"),
                     dcc.RangeSlider(id='vacinacao', min=0, max=10, step=1, value=[2, 5], tooltip={"placement": "bottom", "always_visible": False}, className="card-text" ),
                 ],
                 className="m-1",
@@ -163,9 +162,9 @@ ajuste_parametros = html.Div(
 def ode_sys(t, state, alpha, beta, gamma, delta, mu, nu, t_begv, t_endv):
     s, i, r=state
     vac=nu*heaviside(t-t_begv, 1)*heaviside(t_endv-t, 1)
-    ds_dt=mu*(i+r)-beta*s*i/(s+i+r)-vac*s+delta*r
-    di_dt=beta*s*i/(s+i+r)-gamma*i-mu*i
-    dr_dt=gamma*i-mu*r+vac*s-delta*r
+    ds_dt=mu*(i+r)-beta*s*i/(s+i+r)-vac*s+(12/delta)*r
+    di_dt=beta*s*i/(s+i+r)-(365/gamma)*i-mu*i
+    dr_dt=(365/gamma)*i-mu*r+vac*s-(12/delta)*r
     return [ds_dt, di_dt, dr_dt]
 
 @app.callback(Output('population_chart', 'figure'),
@@ -199,7 +198,7 @@ def gera_grafico(s_init, i_init, r_init, alpha, beta, gamma, delta, nu, vacinaca
     fig.add_trace(go.Scatter(x=sol.t, y=sol.y[2]/(sol.y[0]+sol.y[1]+sol.y[2])*100, name='Recuperado',
                              line=dict(color='#0000ff', width=4)))
     fig.update_layout(title='Dinâmica Modelo SIRS com vacinação',
-                       xaxis_title='Tempo',
+                       xaxis_title='Tempo (anos)',
                        yaxis_title='Proporção da população')
     return fig
 
